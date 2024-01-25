179f2be2262bd65adc9eb3f3a4e830e15b9c3b03
Thaavet <148262279+Thaavet@users.noreply.github.com>
2024-01-25 16:34:29 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,22 @@
 import React from 'react'
-
 export const Home = () => {
   return (
     <div className='home-container'>
 
      <div className='why'> 
-     <img className='banner' src="../dist/assets/meditations.png" alt="meditation" />
+ <img className='banner' src={process.env.PUBLIC_URL + '/assets/meditations.png'} alt="meditation" />
       <h3> Why Meditation </h3>
      Meditation is an activity that can bring many benefits, such as reducing stress, improving concentration and mood, and increasing self-awareness. For this reason, it is important to create a website that facilitates meditation and offers useful features such as a customizable timer.
 <br />
 </div>
 <div className='what'>
-  <img className='banner' src="../dist/assets/screen.png" alt="Meditapp timer" />
+<img className='banner' src={process.env.PUBLIC_URL +"/dist/assets/screen.png"} alt="Meditapp timer" />
   <h3>What is Meditapp</h3>
 Your meditation website “Meditapp” has been created to help people meditate in an easy and accessible way. The customizable timer allows you to set the duration of the activity according to your needs, so you can meditate effectively and without interruptions. In addition, the website offers a wide range of useful resources for meditation, such as guides, videos, and articles, to help you improve your daily practice.
 <br />
 </div>
 <div className='end'>
-  <img className='banner' src="./dist/assets/screen3.png" alt="benefit of meditation" />
+<img className='banner' src={process.env.PUBLIC_URL +"/dist/assets/screen3.png"} alt="benefit of meditation" />
   <h3> References </h3>
 Meditation is a practice that can bring many benefits to your daily life. According to a study conducted by Brown University, meditation can help reduce stress and anxiety, improve memory and concentration, and increase self-awareness. In addition, meditation can help reduce symptoms of depression and anxiety, improve sleep quality, and increase emotional resilience .
 <br />
